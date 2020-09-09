205e49d2972a61e8711fd0399d3c85f4d2036197
Lauren Stein <lmstein22@gmail.com>
2020-09-09 13:57:00 -0400

    Added schema file

---
+++ schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE gdp(
+	id serial PRIMARY KEY,
+	year VARCHAR(4) NOT NULL,
+	country_code VARCHAR(3) NOT NULL,
+	country_name VARCHAR NOT NULL,
+	gdp INT
+);
+
+CREATE TABLE mass_protest(
+	id serial PRIMARY KEY,
+	country VARCHAR NOT NULL,
+	year VARCHAR(4) NOT NULL,
+	protest int NOT NULL
+);
